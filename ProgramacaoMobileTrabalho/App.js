a20d84d51fafc7d30bd9d3fd81b51099f3a9ae93
alee <penzalessandra1@gmail.com>
2024-05-16 15:03:50 -0300

    Questao 2

--- ProgramacaoMobileTrabalho/App.js
+++ ProgramacaoMobileTrabalho/App.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, TextInput, View, Button, Image, Alert, FlatList } from 'react-native';
+import { StyleSheet, Text, TextInput, View} from 'react-native';
+import { React, useState } from "react";
 import Componente1 from './compnentes/componente1';
+import Componente2 from './compnentes/componente2';
 
 export default function App() {
-  
+  const [peso, setPeso] = useState(0.0);
+  const [altura, setAltura] = useState(0.0);
   return (
     <View style={styles.container}>
       <Componente1 cor="purple" tamanho={45} titulo={"App de IMC"}></Componente1>
-      
+      {/* Segunda questao */}
+      <Componente2 
+        funcao={setPeso}
+        titulo="Peso"
+        sugestao='Ex 75.3'
+        tipoTeclado='numeric' ></Componente2>
+        <Componente2 funcao={setAltura} titulo="altura" sugestao='1.68' tipoTeclado='numeric' ></Componente2>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -19,4 +29,5 @@ const styles = StyleSheet.create({
     justifyContent: 'top',
     width: '100%'
   }
+
 })
--- ProgramacaoMobileTrabalho/compnentes/componente2.js
+++ ProgramacaoMobileTrabalho/compnentes/componente2.js
@@ -2,3 +2,35 @@
 teclado, valor de sugestao). Esse componente deve permitir retornar o valor digitado. Use esse
 componentes duas vezes, uma para a altura e outra para o peso. */
 
+import { React, useState } from "react";
+import { View, StyleSheet, Text, TextInput } from "react-native";
+
+export default function CampoTextoComRetorno(props){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>{props.titulo}</Text>
+            <TextInput 
+                style={styles.textInput}
+                placeholder={props.sugestao}
+                keyboardType={props.tipoTeclado}
+                onChangeText={(texto) => props.funcao(texto) }
+            />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+        width: '85%',
+        alignItems: 'center'
+    },
+    titulo:{
+        fontSize: 20
+    },
+    textInput: {
+      borderWidth: 1,
+      paddingLeft: 20,
+      paddingRight: 20,
+      width:'70%',
+      borderColor: 'black',
+    }
+  });
